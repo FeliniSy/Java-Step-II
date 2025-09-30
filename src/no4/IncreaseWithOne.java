231f09b3d8fe1b619d145c2bf2553c253818deb0
FELINISY <ninikheladze9@gmail.com>
2025-09-30 16:49:33 +0400

    Task No.4 Increase Last with One

---
+++ src/no4/IncreaseWithOne.java
@@ -0,0 +1,25 @@
+package no4;
+
+import java.util.Arrays;
+
+public class IncreaseWithOne {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        StringBuilder sb = new StringBuilder();
+
+        for (int i : arr) {
+            sb.append(i);
+        }
+
+        int n = Integer.parseInt(sb.toString()) + 1;
+        System.out.println(n);
+        int[] newArr = new int[arr.length];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int k = n % 10;
+            newArr[i] = k;
+            n /= 10;
+        }
+        System.out.println(Arrays.toString(newArr));
+    }
+}
